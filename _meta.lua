c166e8da098e2924fa45806513696491fdc0321e
darkivist <kpaulj@protonmail.com>
2026-01-10 19:06:03 -0500

    Initial commit

---
+++ _meta.lua
@@ -0,0 +1,7 @@
+local _ = require("gettext")
+
+return {
+    name = "loc",
+    fullname = _("Library of Congress"),
+    description = _([[Search and download EPUBs from the Library of Congress Digital Collections.]]),
+}
